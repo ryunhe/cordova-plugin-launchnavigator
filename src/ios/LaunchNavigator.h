eac0a6d88d8e95cd5d622fc8ef41f04c442fbbf9
seasu <uoryon@gmail.com>
2015-07-30 12:18:33 +0800

    support iOS navigate and view map

---
+++ src/ios/LaunchNavigator.h
@@ -0,0 +1,15 @@
+//
+//  lalaPlugin.h
+//  hello
+//
+//  Created by seasu on 2015/7/29.
+//
+//
+
+#import <Cordova/CDV.h>
+
+@interface LaunchNavigator : CDVPlugin
+
+- (void)doLaunch:(CDVInvokedUrlCommand*)command;
+
+@end
